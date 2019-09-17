f8a0a8a48743083dc6c03a2605e1f5a141d3d76b
Ethan Collins <ethantcollins98@gmail.com>
2019-09-16 18:57:49 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+    render(){
+        function focusHandler() {
+            console.log('Good!')
+        }
+
+        function blurHandler(){
+            console.log('Hey! Eyes on me!')
+        }
+
+
+        return(
+            <button onFocus={focusHandler} onBlur={blurHandler}></button>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React, { Component } from 'react'
+
+export default class Keypad extends Component {
+
+    render(){
+        function checkKey() {
+            console.log('Entering password...')
+        }
+
+        return(
+            <input onKeyUp={checkKey} type="password"></input>
+        )
+    }
+}

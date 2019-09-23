020bc80c85a931e15f47a7c36f8dede757f7d161
SwanHub <jdprince555@gmail.com>
2019-09-23 04:36:57 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+class EyesOnMe extends Component {
+
+    focusFriend = () => {
+        console.log('Good!')
+    }
+
+    payAttention = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+
+    render(){
+        return(
+            <div>
+                <button onFocus={this.focusFriend} onBlur={this.payAttention}>
+                    Eyes on me!
+                </button>
+            </div>
+        )
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
-// Code Keypad Component Here
+import React, { Component } from 'react'
+
+class Keypad extends Component {
+
+    listenForPassword = () => {
+        console.log('Entering password...')
+    }
+
+    render(){
+        return(
+            <div>
+                <input type="password" onKeyUp={this.listenForPassword}/>
+            </div>
+        )
+    }
+}
+
+export default Keypad;
